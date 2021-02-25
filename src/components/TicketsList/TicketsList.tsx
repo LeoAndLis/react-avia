da90814a3ee73fcad0a10d123576b2fe5b826983
LeoAndLis <lis.and.leo@gmail.com>
2021-02-25 15:32:44 +0300

    Add antd, date-fns, finish formatting, refactoring

--- src/components/TicketsList/TicketsList.tsx
+++ src/components/TicketsList/TicketsList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import classes from './TicketsList.module.scss';
 import Ticket from '../Ticket/Ticket';
+
+import classes from './TicketsList.module.scss';
 
 const TicketsList = (props: any) => {
 
